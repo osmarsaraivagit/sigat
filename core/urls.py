3bb86093398dadabbd82ef92ec6c264410b7b4e2
Osmar Saraiva Jr <osmar.saraiva@gmail.com>
2023-12-10 22:58:20 -0300

    localidades com validação

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, lista_localidades
+from .views import home, lista_localidades, localidade_novo, localidade_update
 
 
 urlpatterns = [
     path('home', home, name='home'), #url home, chama view home, cujo nome é 'home'
     path('lista_localidades', lista_localidades, name='lista_localidades'),
+    path('localidade_novo', localidade_novo, name='localidade_novo'),
+    path('localidade_update/<int:id>', localidade_update, name='localidade_update'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Localidades
 from .forms import LocalidadesForm
 
@@ -12,3 +13,35 @@ def lista_localidades(request):
     localidades = Localidades.objects.all().order_by('cidade')
     return render(request, 'core/lista_localidades.html', {'form': form, 'localidades': localidades})
 
+def localidade_novo(request):
+    if request.method == "POST":
+        cidade = request.POST.get('cidade') 
+        estado = request.POST.get('estado') 
+        count_1= Localidades.objects.filter(cidade=cidade).count()
+        count_2= Localidades.objects.filter(estado=estado).count()
+        if count_1 and count_2 > 0:
+                messages.error(request, "Registro já cadastrado com estes mesmos nomes!")
+                return redirect('localidade_novo')
+        else:
+            form = LocalidadesForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('lista_localidades')
+    else:
+        form = LocalidadesForm
+        return render(request, 'core/localidade_novo.html', {'form':form})
+    
+    
+def localidade_update(request, id):
+    localidade = Localidades.objects.get(id=id)
+    form = LocalidadesForm(request.POST or None, instance=localidade)
+    data={}
+    data['localidade'] = localidade
+    data['form'] = form
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('lista_localidades')
+    else:
+        form = LocalidadesForm
+        return render(request, 'core/localidade_update.html', data)
