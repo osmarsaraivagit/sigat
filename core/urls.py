cf384e84357375a592f34b561e9526e01ac2a791
Osmar Saraiva Jr <osmar.saraiva@gmail.com>
2024-08-11 12:09:49 -0300

    first commit

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,126 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, lista_localidades
-
+from .views import home, lista_localidades, localidade_novo, localidade_update, localidade_search, localidade_delete
 
 urlpatterns = [
     path('home', home, name='home'), #url home, chama view home, cujo nome é 'home'
-    path('lista_localidades', lista_localidades, name='lista_localidades'),
+    path('core/lista_localidades', lista_localidades, name='lista_localidades'),
+    path('localidade_novo', localidade_novo, name='localidade_novo'),
+    path('localidade_update/<int:id>', localidade_update, name='localidade_update'),
+    path('localidade_search', localidade_search, name='localidade_search'),
+    #path('delete/<int:pk>', LocalidadeDeleteView.as_view(), name='localidade_delete'),
+    #path('localidades_confirm_delete', localidades_confirm_delete, name='localidades_confirm_delete'), 
+    path('localidade_delete/<int:id>', localidade_delete, name='localidade_delete'),
+    
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,13 +1,87 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse, HttpRequest
+from django.core.paginator import Paginator
 from .models import Localidades
 from .forms import LocalidadesForm
-
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
 
 
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, 'localidades/home.html')
+  
+def localidades_confirm_delete(request):
+    return render(request, 'localidades/localidades_confirm_delete.html')
+
 
 def lista_localidades(request):
     form = LocalidadesForm
-    localidades = Localidades.objects.all().order_by('nome')
+    localidades_list = Localidades.objects.all().order_by('cidade')
+    paginator = Paginator(localidades_list, 5)
+    page = request.GET.get('page')
+    localidades = paginator.get_page(page)
+    data = {}
+    data['localidades'] = localidades
+    data['form'] = form
+    return render(request, 'localidades/lista_localidades.html', data)
 
+
+def localidade_novo(request):
+    if request.method == "POST":
+        cidade = request.POST.get('cidade') 
+        estado = request.POST.get('estado') 
+        count_1= Localidades.objects.filter(cidade=cidade).count()
+        count_2= Localidades.objects.filter(estado=estado).count()
+        if count_1 and count_2 > 0:
+                messages.error(request, "Registro já cadastrado com estes mesmos nomes!")
+                return redirect('localidade_novo')
+        else:
+            form = LocalidadesForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('lista_localidades')
+    else:
+        form = LocalidadesForm
+        return render(request, 'localidades/localidade_novo.html', {'form':form})
+    
+    
+def localidade_update(request, id):
+    localidade = Localidades.objects.get(id=id)
+    form = LocalidadesForm(request.POST or None, instance=localidade)
+    data={}
+    data['localidade'] = localidade
+    data['form'] = form
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registro atualizado com Sucesso!")
+            return redirect('lista_localidades')
+    else:
+        form = LocalidadesForm
+        return render(request, 'localidades/localidade_update.html', data)
+    
+    
+def localidade_search(request):
+        search = request.GET.get('search')
+        localidade = Localidades.objects.filter(cidade__icontains=search)
+        form = LocalidadesForm()
+        data = {}
+        data['localidades'] = localidade
+        data['form'] = form
+        return render (request, 'localidades/lista_localidades.html', data)
+    
+     
+def localidade_delete(request, id):
+    localidade = Localidades.objects.get(id=id)
+    localidade.delete()
+    messages.success(request, "Registro Excluído com Sucesso!")
+    return redirect('lista_localidades')
+
+
+#class LocalidadeDeleteView(DeleteView):
+   # model = Localidades
+    #success_url= reverse_lazy(
+        #"lista_localidade"
+    #)
+ 
